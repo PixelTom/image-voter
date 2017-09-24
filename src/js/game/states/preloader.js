50c535e8efedda141d2af3f3b0c3bc932dc4aeb7
PixelTom <tom@pixeltom.net>
2017-09-24 16:13:58 +1000

    Old pool preload commented out.

--- src/js/game/states/preloader.js
+++ src/js/game/states/preloader.js
@@ -3,10 +3,10 @@ let picArray = [];
 
 preloader.preload = function () {
   for(let i = 1; i <= 20; i++)
-  {
+  /*{ Old Pool, no longer used.
     this.game.load.image('pic' + String(i), 'images/pool/' + String(i) + '.jpg');
     picArray.push('pic' + String(i));
-  }
+  }*/
   this.game.load.image('logo', 'images/phaser.png');
 };
 
