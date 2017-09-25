ae2e857ecf3ec9a8502c2b150fb756de706a417c
PixelTom <tom@pixeltom.net>
2017-09-25 23:09:07 +1000

    Preload buttons.

--- src/js/game/states/preloader.js
+++ src/js/game/states/preloader.js
@@ -2,11 +2,9 @@ const preloader = {};
 let picArray = [];
 
 preloader.preload = function () {
-  for(let i = 1; i <= 20; i++)
-  /*{ Old Pool, no longer used.
-    this.game.load.image('pic' + String(i), 'images/pool/' + String(i) + '.jpg');
-    picArray.push('pic' + String(i));
-  }*/
+  this.game.load.image('btnDraw', 'images/draw.png')
+  this.game.load.image('btnWin', 'images/win.png')
+  this.game.load.image('btnPurge', 'images/purge.png')
   this.game.load.image('logo', 'images/phaser.png');
 };
 
