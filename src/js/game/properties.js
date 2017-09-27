acd3f6ea689ef9daccb036eca4ad4accf92a4a70
Tom Gattenhof <tom@pixeltom.net>
2017-09-27 18:43:46 +1000

    The name!

--- src/js/game/properties.js
+++ src/js/game/properties.js
@@ -1,7 +1,7 @@
 var npmProperties = require('../../../package.json');
 
 module.exports = {
-  title: 'Phaser JS Boilerplate',
+  title: 'Image Voter',
   description: npmProperties.description,
   port: 3017,
   liveReloadPort: 3018,
